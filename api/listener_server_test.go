547047b9bed09f1697d91639b081b92bf770eb6e
Denis Mishin <dmishin@pomerium.com>
2022-01-07 12:25:22 -0500

    add delete inactive listener record test

--- api/listener_server_test.go
+++ api/listener_server_test.go
@@ -29,16 +29,16 @@ func TestListenerServer(t *testing.T) {
 		},
 	})
 	require.NoError(t, err)
-	id := rec.GetId()
-	require.NotEmpty(t, id)
+	idA := rec.GetId()
+	require.NotEmpty(t, idA)
 
 	status, err := srv.Update(ctx, &pb.ListenerUpdateRequest{
-		ConnectionIds: []string{id},
+		ConnectionIds: []string{idA},
 		Connected:     true,
 	})
 	require.NoError(t, err)
 
-	cs, there := status.Listeners[id]
+	cs, there := status.Listeners[idA]
 	require.True(t, there)
 	require.NotNil(t, cs.ListenAddr)
 	require.True(t, cs.Listening)
@@ -51,46 +51,46 @@ func TestListenerServer(t *testing.T) {
 	for k, sel := range map[string]*pb.Selector{
 		"all":    {All: true},
 		"by tag": {Tags: []string{"test"}},
-		"by id":  {Ids: []string{id}},
+		"by id":  {Ids: []string{idA}},
 	} {
 		status, err = srv.GetStatus(ctx, sel)
-		if assert.NoError(t, err, k) && assert.Contains(t, status.Listeners, id, k) {
-			if assert.NotNil(t, status.Listeners[id].ListenAddr, k) {
-				assert.Equal(t, listenAddr, *status.Listeners[id].ListenAddr, k)
+		if assert.NoError(t, err, k) && assert.Contains(t, status.Listeners, idA, k) {
+			if assert.NotNil(t, status.Listeners[idA].ListenAddr, k) {
+				assert.Equal(t, listenAddr, *status.Listeners[idA].ListenAddr, k)
 			}
-			assert.True(t, status.Listeners[id].Listening, k)
-			assert.Nil(t, status.Listeners[id].LastError, k)
+			assert.True(t, status.Listeners[idA].Listening, k)
+			assert.Nil(t, status.Listeners[idA].LastError, k)
 		}
 	}
 
 	// update should be idempotent
 	status, err = srv.Update(ctx, &pb.ListenerUpdateRequest{
-		ConnectionIds: []string{id},
+		ConnectionIds: []string{idA},
 		Connected:     true,
 	})
-	if assert.NoError(t, err) && assert.Contains(t, status.Listeners, id) {
-		assert.True(t, status.Listeners[id].Listening)
-		if assert.NotNil(t, status.Listeners[id].ListenAddr) {
-			assert.Equal(t, listenAddr, *status.Listeners[id].ListenAddr)
+	if assert.NoError(t, err) && assert.Contains(t, status.Listeners, idA) {
+		assert.True(t, status.Listeners[idA].Listening)
+		if assert.NotNil(t, status.Listeners[idA].ListenAddr) {
+			assert.Equal(t, listenAddr, *status.Listeners[idA].ListenAddr)
 		}
-		assert.Nil(t, status.Listeners[id].LastError)
+		assert.Nil(t, status.Listeners[idA].LastError)
 	}
 
 	status, err = srv.Update(ctx, &pb.ListenerUpdateRequest{
-		ConnectionIds: []string{id},
+		ConnectionIds: []string{idA},
 		Connected:     false,
 	})
-	if assert.NoError(t, err) && assert.Contains(t, status.Listeners, id) {
-		assert.False(t, status.Listeners[id].Listening)
-		assert.Nil(t, status.Listeners[id].ListenAddr)
-		assert.Nil(t, status.Listeners[id].LastError)
+	if assert.NoError(t, err) && assert.Contains(t, status.Listeners, idA) {
+		assert.False(t, status.Listeners[idA].Listening)
+		assert.Nil(t, status.Listeners[idA].ListenAddr)
+		assert.Nil(t, status.Listeners[idA].LastError)
 	}
 
 	status, err = srv.GetStatus(ctx, &pb.Selector{All: true})
-	if assert.NoError(t, err) && assert.Contains(t, status.Listeners, id) {
-		assert.False(t, status.Listeners[id].Listening)
-		assert.Nil(t, status.Listeners[id].ListenAddr)
-		assert.Nil(t, status.Listeners[id].LastError)
+	if assert.NoError(t, err) && assert.Contains(t, status.Listeners, idA) {
+		assert.False(t, status.Listeners[idA].Listening)
+		assert.Nil(t, status.Listeners[idA].ListenAddr)
+		assert.Nil(t, status.Listeners[idA].LastError)
 	}
 
 	// ensure listener is shut down
@@ -148,11 +148,26 @@ func TestDeleteActiveListener(t *testing.T) {
 		return !canListen()
 	}, time.Second*2, time.Millisecond*100, "listener should be up")
 
+	rec, err = srv.Upsert(ctx, &pb.Record{
+		Tags: []string{"test"},
+		Conn: &pb.Connection{
+			RemoteAddr: "notactive.localhost.pomerium.io:99",
+			ListenAddr: proto.String(":0"),
+		},
+	})
+	require.NoError(t, err)
+	idB := rec.GetId()
+	require.NotEmpty(t, idB)
+
 	_, err = srv.Delete(ctx, &pb.Selector{
-		Ids: []string{id},
+		Ids: []string{id, idB},
 	})
 	require.NoError(t, err, "can delete")
 
 	// listener should be down
 	require.Eventually(t, canListen, time.Second*2, time.Millisecond*100, "listener should be down")
+
+	recs, err := srv.List(ctx, &pb.Selector{All: true})
+	require.NoError(t, err, "fetch records")
+	require.Empty(t, recs.Records)
 }
