cefe71a3cbd46006b5d83bc70ebf7eafd73fdd85
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2023-08-25 11:50:38 -0700

    vendor internal/signer from enterprise-certificate-proxy
    Vendor some files from the internal/signer directory in
    https://github.com/googleapis/enterprise-certificate-proxy/ into a new
    package 'third_party/ecpsigner'.
    
    The enterprise-certificate-proxy code is licensed under the Apache
    License 2.0. These files were copied as of the v0.2.5 tag in that
    repository. No modifications were made to these files.

---
+++ third_party/ecpsigner/darwin/keychain/keychain_test.go
@@ -0,0 +1,48 @@
+// Copyright 2022 Google LLC.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build darwin && cgo
+// +build darwin,cgo
+
+package keychain
+
+import (
+	"bytes"
+	"testing"
+	"unsafe"
+)
+
+func TestKeychainError(t *testing.T) {
+	tests := []struct {
+		e    keychainError
+		want string
+	}{
+		{e: keychainError(0), want: "No error."},
+		{e: keychainError(-4), want: "Function or operation not implemented."},
+	}
+
+	for i, test := range tests {
+		if got := test.e.Error(); got != test.want {
+			t.Errorf("test %d: %#v.Error() = %q, want %q", i, test.e, got, test.want)
+		}
+	}
+}
+
+func TestBytesToCFDataRoundTrip(t *testing.T) {
+	want := []byte("an arbitrary and yet coherent byte slice!")
+	d := bytesToCFData(want)
+	defer cfRelease(unsafe.Pointer(d))
+	if got := cfDataToBytes(d); !bytes.Equal(got, want) {
+		t.Errorf("bytesToCFData -> cfDataToBytes\ngot  %x\nwant %x", got, want)
+	}
+}
