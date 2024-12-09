485091fa0c95e2f0b2ed4d5e07d4cf6c6d5676bd
Caleb Doxsey <cdoxsey@pomerium.com>
2024-12-09 14:28:48 -0700

    wip

--- tunnel/tunnel_http2.go
+++ tunnel/tunnel_http2.go
@@ -5,11 +5,15 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"net/url"
 
+	"github.com/dunglas/httpsfv"
+	"github.com/quic-go/quic-go/http3"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/net/http2"
+	"golang.org/x/sync/errgroup"
 )
 
 type http2tunneler struct {
@@ -26,39 +30,16 @@ func (t *http2tunneler) TunnelTCP(
 
 	eventSink.OnConnecting(ctx)
 
+	cc, err := t.getClientConn(ctx)
+	if err != nil {
+		return err
+	}
+	defer cc.Close()
+
 	hdr := http.Header{}
 	if rawJWT != "" {
 		hdr.Set("Authorization", "Pomerium "+rawJWT)
 	}
-
-	if t.cfg.tlsConfig == nil {
-		return fmt.Errorf("%w: http2 requires TLS", errUnsupported)
-	}
-
-	cfg := t.cfg.tlsConfig.Clone()
-	cfg.NextProtos = []string{"h2"}
-
-	raw, err := (&tls.Dialer{Config: cfg}).DialContext(ctx, "tcp", t.cfg.proxyHost)
-	if err != nil {
-		return fmt.Errorf("http/2: failed to establish connection to proxy: %w", err)
-	}
-	defer func() { _ = raw.Close() }()
-
-	remote, ok := raw.(*tls.Conn)
-	if !ok {
-		return fmt.Errorf("http/2: unexpected connection type returned from dial: %T", raw)
-	}
-
-	protocol := remote.ConnectionState().NegotiatedProtocol
-	if protocol != "h2" {
-		return fmt.Errorf("%w: unexpected TLS protocol: %s", errUnsupported, protocol)
-	}
-
-	cc, err := (&http2.Transport{}).NewClientConn(remote)
-	if err != nil {
-		return fmt.Errorf("http/2: failed to establish connection: %w", err)
-	}
-	defer cc.Close()
 
 	pr, pw := io.Pipe()
 
@@ -111,4 +92,132 @@ func (t *http2tunneler) TunnelTCP(
 	eventSink.OnDisconnected(ctx, err)
 
 	return err
+}
+
+func (t *http2tunneler) TunnelUDP(
+	ctx context.Context,
+	eventSink EventSink,
+	local UDPPacketReaderWriter,
+	rawJWT string,
+) error {
+	ctx = log.Ctx(ctx).With().Str("component", "http2tunneler").Logger().WithContext(ctx)
+
+	eventSink.OnConnecting(ctx)
+
+	cc, err := t.getClientConn(ctx)
+	if err != nil {
+		return err
+	}
+	defer cc.Close()
+
+	pr, pw := io.Pipe()
+
+	req, err := buildExtendedConnectUDPRequest(ctx, t.cfg.proxyHost, t.cfg.dstHost, rawJWT, pr)
+	if err != nil {
+		return fmt.Errorf("http/2: failed to build extended connect udp request: %w", err)
+	}
+
+	res, err := cc.RoundTrip(req)
+	if err != nil {
+		return fmt.Errorf("http/2: error making connect udp request: %w", err)
+	}
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	case http.StatusOK:
+	case http.StatusServiceUnavailable:
+		return errUnavailable
+	case http.StatusMovedPermanently,
+		http.StatusFound,
+		http.StatusTemporaryRedirect,
+		http.StatusPermanentRedirect:
+		return errUnauthenticated
+	default:
+		return fmt.Errorf("http/2: invalid http response code: %d", res.StatusCode)
+	}
+
+	eventSink.OnConnected(ctx)
+
+	eg, ectx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		return streamFromCapsuleDatagramsToUDPPacketWriter(ectx, local, res.Body)
+	})
+	eg.Go(func() error {
+		return streamFromUDPPacketReaderToCapsuleDatagrams(ectx, pw, local)
+	})
+	err = eg.Wait()
+
+	eventSink.OnDisconnected(ctx, err)
+
+	return err
+}
+
+func (t *http2tunneler) getClientConn(
+	ctx context.Context,
+) (*http2.ClientConn, error) {
+	if t.cfg.tlsConfig == nil {
+		return nil, fmt.Errorf("%w: http2 requires TLS", errUnsupported)
+	}
+	cfg := t.cfg.tlsConfig.Clone()
+	cfg.NextProtos = []string{"h2"}
+
+	raw, err := (&tls.Dialer{Config: cfg}).DialContext(ctx, "tcp", t.cfg.proxyHost)
+	if err != nil {
+		return nil, fmt.Errorf("http/2: failed to establish connection to proxy: %w", err)
+	}
+	defer func() { _ = raw.Close() }()
+
+	remote, ok := raw.(*tls.Conn)
+	if !ok {
+		return nil, fmt.Errorf("http/2: unexpected connection type returned from dial: %T", raw)
+	}
+
+	protocol := remote.ConnectionState().NegotiatedProtocol
+	if protocol != "h2" {
+		return nil, fmt.Errorf("%w: unexpected TLS protocol: %s", errUnsupported, protocol)
+	}
+
+	cc, err := (&http2.Transport{}).NewClientConn(remote)
+	if err != nil {
+		return nil, fmt.Errorf("http/2: failed to establish connection: %w", err)
+	}
+
+	return cc, nil
+}
+
+func buildExtendedConnectUDPRequest(
+	ctx context.Context,
+	proxyHost, dstHost string,
+	rawJWT string,
+	body io.ReadCloser,
+) (*http.Request, error) {
+	capsuleProtocolHeaderValue, err := httpsfv.Marshal(httpsfv.NewItem(true))
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode capsule protocol header value")
+	}
+
+	hdr := http.Header{
+		":protocol":                 {"connect-udp"},
+		":scheme":                   {"https"},
+		http3.CapsuleProtocolHeader: {capsuleProtocolHeaderValue},
+	}
+	if rawJWT != "" {
+		hdr.Set("Authorization", "Pomerium "+rawJWT)
+	}
+	dstHost, dstPort, err := net.SplitHostPort(dstHost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to split destination host into host and port")
+	}
+	u, err := url.Parse(fmt.Sprintf("https://%s/.well-known/masque/udp/%s/%s/", proxyHost, dstHost, dstPort))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create destination url: %w", err)
+	}
+	return (&http.Request{
+		Method:        http.MethodConnect,
+		URL:           u,
+		Host:          proxyHost,
+		Header:        hdr,
+		ContentLength: -1,
+		Body:          body,
+	}).WithContext(ctx), nil
 }
--- tunnel/tunnel_udp.go
+++ tunnel/tunnel_udp.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"net/netip"
 	"time"
 
@@ -63,8 +64,8 @@ func (tun *Tunnel) RunUDPListener(ctx context.Context, listenerAddress string) e
 }
 
 func (tun *Tunnel) RunUDPSessionManager(ctx context.Context, conn *net.UDPConn) error {
+	tunneler := tun.pickUDPTunneler(ctx)
 	return newUDPSessionManager(conn, func(ctx context.Context, urw UDPPacketReaderWriter) error {
-		tunneler := &http1tunneler{cfg: tun.cfg}
 		eventSink := LogEvents()
 		return tun.runWithJWT(ctx, eventSink, func(ctx context.Context, rawJWT string) error {
 			// always disconnect after 10 minutes
@@ -74,6 +75,48 @@ func (tun *Tunnel) RunUDPSessionManager(ctx context.Context, conn *net.UDPConn) 
 			return tunneler.TunnelUDP(ctx, eventSink, urw, rawJWT)
 		})
 	}).run(ctx)
+}
+
+// pickUDPTunneler picks a udp tunneler for the given proxy.
+func (tun *Tunnel) pickUDPTunneler(ctx context.Context) UDPTunneler {
+	ctx = log.Ctx(ctx).With().Str("component", "pick-udp-tunneler").Logger().WithContext(ctx)
+
+	fallback := &http1tunneler{cfg: tun.cfg}
+
+	// if we're not using TLS, only HTTP1 is supported
+	if tun.cfg.tlsConfig == nil {
+		log.Ctx(ctx).Info().Msg("tls not enabled, using http1")
+		return fallback
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			ForceAttemptHTTP2: true,
+			TLSClientConfig:   tun.cfg.tlsConfig,
+		},
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://"+tun.cfg.proxyHost, nil)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("failed to create probe request, falling back to http1")
+		return fallback
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("failed to make probe request, falling back to http1")
+		return fallback
+	}
+	res.Body.Close()
+
+	// TODO: add http3 support
+	if res.ProtoMajor == 2 {
+		log.Ctx(ctx).Info().Msg("using http2")
+		return &http2tunneler{cfg: tun.cfg}
+	}
+
+	log.Ctx(ctx).Info().Msg("using http1")
+	return fallback
 }
 
 type udpSessionHandler func(context.Context, UDPPacketReaderWriter) error
